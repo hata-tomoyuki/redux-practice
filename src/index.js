30ff7c96d1c551024d04658e7a9ae4ce9f5e269f
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-17 17:26:37 +0900

    create dispatch, reducer, store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createStore } from "redux"
+
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  };
+};
+
+let store = createStore(counterReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
