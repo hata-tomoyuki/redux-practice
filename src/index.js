a567528ce9ff92e03a5b38a78c29555ba295a24f
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-17 17:48:33 +0900

    refactoring

--- src/index.js
+++ src/index.js
@@ -3,36 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
+import allReducers from './reducers';
 
-const increment = () => {
-  return {
-    type: "INCREMENT",
-  };
-};
-
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-  };
-};
-
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-  };
-};
-
-let store = createStore(counterReducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-store.dispatch(increment());
+const store = createStore(allReducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
