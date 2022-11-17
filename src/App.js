8d24e2dd8580922a3dc1fb867f54aa01cd116131
hata-tomoyuki <h.tomo0602@gmail.com>
2022-11-17 19:05:09 +0900

    use provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogin = useSelector((state) => state.isLogin);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <h2>カウント：{counter}</h2>
+      {isLogin ? <h3>ログインに成功！</h3> : <h3>ログインしてください</h3>}
     </div>
   );
 }
